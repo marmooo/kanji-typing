bddac20175e30c79d17be191448e7e2f4debdcdd
marmooo <marmooo@users.noreply.github.com>
2022-05-05 08:59:10 +0900

    add OGP

--- src/sw.js
+++ src/sw.js
@@ -1,4 +1,4 @@
-var CACHE_NAME = "2022-04-18 09:40";
+var CACHE_NAME = "2022-05-05 09:00";
 var urlsToCache = [
   "/kanji-typing/",
   "/kanji-typing/index.js",
@@ -7,7 +7,7 @@ var urlsToCache = [
   "/kanji-typing/mp3/correct.mp3",
   "/kanji-typing/mp3/end.mp3",
   "/kanji-typing/mp3/keyboard.mp3",
-  "/kanji-typing/favicon/original.svg",
+  "/kanji-typing/favicon/favicon.svg",
   "https://marmooo.github.io/fonts/textar-light.woff2",
   "https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css",
   "https://cdn.jsdelivr.net/npm/simple-keyboard@3.4.52/build/index.min.js",
