44c1b3ae7a3e06c4b2e691b542d1003cf26f2a3c
marmooo <marmooo@users.noreply.github.com>
2023-06-17 12:59:12 +0900

    update

--- src/sw.js
+++ src/sw.js
@@ -1,4 +1,4 @@
-var CACHE_NAME = "2023-05-23 09:45";
+var CACHE_NAME = "2023-06-17 13:00";
 var urlsToCache = [
   "/kanji-typing/",
   "/kanji-typing/index.js",
@@ -9,7 +9,6 @@ var urlsToCache = [
   "/kanji-typing/mp3/keyboard.mp3",
   "/kanji-typing/favicon/favicon.svg",
   "https://marmooo.github.io/fonts/textar-light.woff2",
-  "https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css",
   "https://cdn.jsdelivr.net/npm/simple-keyboard@3.4.52/build/index.min.js",
 ];
 
